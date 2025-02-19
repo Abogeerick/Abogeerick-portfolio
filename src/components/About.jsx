775360d60455ba848a6a211c6620bb6f7d3d06d5
Abogeerick <abogeerick@gmail.com>
2025-02-19 15:20:03 +0300

    chore: minor updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -36,14 +36,14 @@ const About = () => {
             className="md:w-2/3 text-center md:text-left"
           >
             <p className="text-gray-600 dark:text-gray-300 mb-4">
-              Hi, I’m Erick Aboge, a dedicated Software Engineer with a strong background in both Quality Assurance and front-end development. Currently, I work on ERP systems at Optiven Ltd, where I focus on building scalable, cross-platform mobile applications using Flutter and ensuring high-quality, bug-free software through comprehensive testing. I am passionate about developing seamless, user-friendly applications and optimizing systems to meet both functional and performance standards
-            </p>
-            <p className="text-gray-600 dark:text-gray-300 mb-4">
-              I have a Bachelor's Degree in Finance/Information Technology from the University of Nairobi and a Software Development Certificate from Moringa School. My experience spans across both front-end and back-end development, with a particular interest in creating intuitive and engaging user interfaces.
-            </p>
-            <p className="text-gray-600 dark:text-gray-300">
-              Outside of coding, I love exploring new technologies, working on freelance projects, and contributing to open-source communities. I’m always eager to learn and grow, whether it’s through new tools or frameworks.
-            </p>
+  Hi, I’m Erick Aboge, a dedicated Fullstack Developer with over 3 years of experience in designing and implementing scalable web applications. Currently, I work at Optiven Ltd, focusing on developing and maintaining ERP systems using Vue.js, React.js, Node.js, and MongoDB. I have hands-on experience customizing Shopify themes, developing custom apps, and integrating APIs to enhance e-commerce functionalities. I am passionate about creating seamless, user-friendly applications that drive business growth.
+              </p>
+              <p className="text-gray-600 dark:text-gray-300 mb-4">
+                I hold a Bachelor's Degree in Commerce with a specialization in Information Technology from the University of Nairobi and a Software Development Certificate from Moringa School. My expertise spans both front-end and back-end development, with a particular interest in building intuitive user interfaces and robust server-side applications.
+              </p>
+              <p className="text-gray-600 dark:text-gray-300">
+                Outside of coding, I enjoy exploring new technologies, working on freelance projects, and contributing to open-source communities. I am always eager to learn and grow, continually seeking opportunities to enhance my skills and stay abreast of industry trends.
+              </p>
           </motion.div>
         </div>
       </div>
